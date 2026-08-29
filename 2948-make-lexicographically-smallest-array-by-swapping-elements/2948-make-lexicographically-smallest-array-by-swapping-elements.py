11c5c2dbeb81b4cbc3db0c3e2ee726b0cd4edd60
Anik Majumder <anikmder2005@gmail.com>
2026-08-29 19:28:07 +0530

    Time: 213 ms (91.79%), Space: 52.4 MB (45.52%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.py
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,28 +1,17 @@
-from typing import List
-from collections import deque
-
 class Solution:
-    def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
-        nums_sorted = sorted(nums)
+    def lexicographicallySmallestArray(self, A: list[int], limit: int) -> list[int]:
+        groups = []
+        gmap = {}
 
-        curr_group = 0
-        num_to_group = {}
-        group_to_list = {}
+        for val in sorted(A):
+            if not groups or val - groups[-1][-1] > limit:
+                groups.append([])
+            groups[-1].append(val)
+            gmap[val] = len(groups) - 1
 
-        num_to_group[nums_sorted[0]] = curr_group
-        group_to_list[curr_group] = deque([nums_sorted[0]])
+        itr = [iter(g) for g in groups]
 
-        for i in range(1, len(nums_sorted)):
-            if abs(nums_sorted[i] - nums_sorted[i - 1]) > limit:
-                curr_group += 1
-                group_to_list[curr_group] = deque()
+        for i in range(len(A)):
+            A[i] = next(itr[gmap[A[i]]])
 
-            num_to_group[nums_sorted[i]] = curr_group
-            group_to_list[curr_group].append(nums_sorted[i])
-
-        for i in range(len(nums)):
-            num = nums[i]                  # FIX
-            group = num_to_group[num]
-            nums[i] = group_to_list[group].popleft()
-
-        return nums
+        return A
