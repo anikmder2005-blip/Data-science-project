eda0cefa9b764c671f40748fc4b8d74c08bc5d2a
Anik Majumder <anikmder2005@gmail.com>
2026-09-21 19:38:00 +0530

    Time: 168 ms (38.39%), Space: 151.1 MB (70.54%) - LeetHub

--- 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
@@ -1,43 +1,30 @@
 class Solution {
 public:
-    using ll = long long;
+    vector<long long> resultArray(vector<int>& nums, int k) {
+        vector<long long> ans(k, 0);
+        vector<long long> dp(k, 0);
 
-    int n;
+        for (int num : nums) {
+            vector<long long> newDp(k, 0);
+            int mod = num % k;
 
-    ll solve(int i, int prevProd, int req, int k, auto& nums, auto& dp) {
+            // Start a new subarray containing only num
+            newDp[mod] = 1;
 
-        if(i >= nums.size()) return 0;
-        if(dp[i][prevProd] != -1) return dp[i][prevProd];
+            // Extend all previous subarrays
+            for (int r = 0; r < k; r++) {
+                int newMod = (1LL * r * mod) % k;
+                newDp[newMod] += dp[r];
+            }
 
-        ll skip = 0, take = 0;
+            // Add counts to answer
+            for (int r = 0; r < k; r++) {
+                ans[r] += newDp[r];
+            }
 
-        // Skip current element (only if no subarray started yet)
-        if(prevProd == k){
-            skip = solve(i + 1, k, req, k, nums, dp);
+            dp = move(newDp);
         }
 
-        // Take current element
-        ll curProd;
-        if(prevProd == k) curProd = nums[i]; // started from cur num
-        else curProd = (prevProd * nums[i]) % k; // extending forward
-
-        take += (curProd == req);
-        take += solve(i + 1, curProd, req, k, nums, dp);
-
-        return dp[i][prevProd] = take + skip;
-    }
-
-    vector<ll> resultArray(vector<int>& nums, int k) {
-        n = nums.size();
-        vector<ll> res(k, 0);
-
-        for(int& num : nums) num %= k;
-
-        for(int r = 0; r < k; r++){
-            vector<vector<ll>> dp(n, vector<ll>(k + 1, -1));
-            res[r] = solve(0, k, r, k, nums, dp);
-        }
-
-        return res;
+        return ans;
     }
 };
